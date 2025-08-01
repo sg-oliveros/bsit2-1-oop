eac906df79e3b63e62074dba8f5c366645eafe89
Samantha Gayle Oliveros <sgoliveros64460@liceo.edu.ph>
2025-08-01 11:25:57 +0800

    lab-activity-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main{
-    public static void main(String[]args){
-        System.out.println("Main Branch!");
+import java.util.Scanner;
+
+public class Main {
+    static int number = 0;
+
+    public static void main(String[] args) {
+
+        System.out.println("Please input 5 numbers.");
+            Scanner scan = new Scanner(System.in);
+
+            for (int i = 1; i <= 5; i++) {
+                System.out.print("Enter number " + i + ": " );
+                    number = number + scan.nextInt();
+
+        }
+        System.out.println(" ");
+        System.out.println("Total Number: " + number);
+
+        scan.close();
     }
 }
