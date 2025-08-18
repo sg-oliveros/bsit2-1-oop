4a579c4d4fb8cae3da81c2a9957bff67859112a8
Jose Saturnino Malong <jsmalong02705@liceo.edu.ph>
2025-08-18 10:02:20 +0800

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main{
-    public static void main(String[]args){
-        System.out.println("Main Branch!");
+    public static void main(String[] args) {
+        GradeCalculator calculator = new GradeCalculator();
+
+        String studentName = "John Smith";
+        double[] grades = {85.5, 92.0, 78.5, 90.0};
+
+        double average = calculator.calculateAverage(grades);
+
+        String letterGrade = calculator.getLetterGrade(average);
+
+        calculator.displayResult(studentName, average);
+        calculator.displayResult(studentName, average, letterGrade);
     }
 }
