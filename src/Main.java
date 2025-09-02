f190a603ff475110df938213cad965983e96156d
Samantha Gayle Oliveros <sgoliveros64460@liceo.edu.ph>
2025-09-02 19:31:50 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main{
-    public static void main(String[]args){
-        System.out.println("Main Branch!");
+public class Main {
+    public static void main(String[] args) {
+        LibraryManager manager = new LibraryManager();
+        System.out.println("≡ Library Management System ≡");
+
+        manager.showBooks();
+        manager.addBook();
+        manager.addBook();
+        manager.addBook();
+        manager.removeBook();
+        manager.removeBook();
+        manager.removeBook();
+
+        System.out.println("\nProgram completed successfully.");
     }
 }
