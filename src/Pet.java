7239c135a6ed5e70f9347a56433ff7eccef8b7bc
Jose Saturnino Malong <jsmalong02705@liceo.edu.ph>
2025-09-15 10:17:46 +0800

    lab-activity-9

---
+++ src/Pet.java
@@ -0,0 +1,70 @@
+// Interface for trainable pets
+interface Trainable {
+    void performTrick();
+}
+
+// Abstract Pet class
+abstract class Pet {
+    protected String name;
+    protected int age;
+
+    public Pet(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public abstract void makeSound();
+
+    public void displayInfo() {
+        System.out.println("Pet Name: " + name +", " + "Age: " + age);
+        System.out.print(name + " says: ");
+        makeSound();
+        System.out.println();
+    }
+}
+
+// Dog subclass
+class Dog extends Pet implements Trainable {
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Woof! Woof!");
+    }
+
+    @Override
+    public void performTrick() {
+        System.out.println("Training " + name + ": Sits and shakes hands");
+    }
+}
+
+// Cat subclass
+class Cat extends Pet {
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Meow! Meow!");
+    }
+}
+
+// Bird subclass
+class Bird extends Pet implements Trainable {
+    public Bird(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Tweet! Tweet!");
+    }
+
+    @Override
+    public void performTrick() {
+        System.out.println("Training " + name + ": Flies in circles and lands on perch");
+    }
+}
